00f74646409b03849d1ca644d2f1156f0b9131fb
lordlightness <103905932+lordlightness@users.noreply.github.com>
2022-05-19 16:45:44 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -148,7 +148,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-    conn.sendHydrated(m.chat, text.trim(), author, thumbmenu, 'https://github.com/itsmedell', '𝐆𝐢𝐭𝐡𝐮𝐛', `+${global.ownernumber}`, '𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐰𝐧𝐞𝐫', [
+    conn.sendHydrated(m.chat, text.trim(), author, thumbmenu, sig, 'Instagram', `+${global.ownernumber}`, 'Number Owner', [
       ['𝐃𝐨𝐧𝐚𝐭𝐞', '/donasi'],
       ['𝐒𝐩𝐞𝐞𝐝', '/ping'],
       ['𝐂𝐫𝐞𝐚𝐭𝐨𝐫', '/owner']
